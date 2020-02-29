f288ff4312140633dce1941f892c8efbba20b4d5
rbulin <roman.bulinski@pentacomp.pl>
2020-02-29 11:54:26 +0100

    Implemented code

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,9 @@
 public class Solution {
 
     public static boolean solution(String str, String ending) {
+        if( str.substring( (str.length()) - ending.length() ).equals( ending ) ){
+            return true;
+        }
         return false;
     }
 
