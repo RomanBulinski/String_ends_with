d35761a7e98ac6ec4fd247ee8e8cd17427b242f5
rbulin <roman.bulinski@pentacomp.pl>
2020-02-29 12:01:28 +0100

    Added new logic

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,9 @@
 public class Solution {
 
     public static boolean solution(String str, String ending) {
+        if(str.length()<ending.length()){
+            return false;
+        }
         if( str.substring( (str.length()) - ending.length() ).equals( ending ) ){
             return true;
         }
